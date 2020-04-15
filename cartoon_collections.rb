8199468f0391c0d54032ccf5141b7be91703d2b4
Sung Bae <singsang2@gmail.com>
2020-04-15 13:22:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.redcue {|n| n.length > 4}
+  array.reduce {|n| n.length > 4}
 end
 
 def find_the_cheese# code an argument here
