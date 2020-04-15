08404d44dd81a7a2bc35096fb6ed7cb3dda00b71
Sung Bae <singsang2@gmail.com>
2020-04-15 13:18:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |n, index|
+    puts "#{index+1}. #{n}"
+  end
 end
 
 def summon_captain_planet# code an argument here
